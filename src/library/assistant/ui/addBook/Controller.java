417a6c5f6d65a67492e5d1c7bdb5a4bdaf93cc5c
Touhidur Rahman <touhidalamin123@gmail.com>
2020-06-05 15:07:00 +0600

    Project created and add book design done

---
+++ src/library/assistant/ui/addBook/Controller.java
@@ -0,0 +1,38 @@
+package library.assistant.ui.addBook;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private JFXTextField title;
+
+    @FXML
+    private JFXTextField id;
+
+    @FXML
+    private JFXTextField author;
+
+    @FXML
+    private JFXTextField publisher;
+
+    @FXML
+    private JFXButton saveButton;
+
+    @FXML
+    private JFXButton cancelButton;
+
+    @FXML
+    void addBook(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancel(ActionEvent event) {
+
+    }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module LIbrary.Assistant {
+    requires javafx.fxml;
+    requires javafx.controls;
+    opens library.assistant.ui.addBook;
+    requires com.jfoenix;
+}
